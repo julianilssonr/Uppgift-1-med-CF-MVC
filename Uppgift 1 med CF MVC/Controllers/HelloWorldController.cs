3b0fe2198d64fe07da49382b4e39741bdaaf1803
julianilssonr <79918907+julianilssonr@users.noreply.github.com>
2021-06-03 14:44:00 +0200

    Lagt till en controller

---
+++ Uppgift 1 med CF MVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uppgift_1_med_CF_MVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

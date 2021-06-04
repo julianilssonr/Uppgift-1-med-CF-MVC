161360a09469bdc9610e34a5daae5acd32e17866
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2021-06-04 14:37:48 +0200

    ljsfdndslvnkfdslf

---
+++ Uppgift 2 MVC med CF/Models/CalculatorModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uppgift_2_MVC_med_CF.Models
+{
+    public class CalculatorModel
+    {
+        [Required]
+        public decimal ValueA { get; set; }
+        [Required]
+        public decimal ValueB { get; set; }
+    }
+}
